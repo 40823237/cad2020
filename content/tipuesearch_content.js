dfd8b4e6f2454f00d8fb46ff8fb6422857bc4765
40823237 <40823237@gm.nfu.edu.tw>
2020-10-19 17:44:10 +0800

    add home and work page

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'Home', 'text': '\n', 'tags': '', 'url': 'Home.html'}, {'title': 'Work', 'text': '', 'tags': '', 'url': 'Work.html'}, {'title': 'Chart', 'text': '', 'tags': '', 'url': 'Chart.html'}, {'title': 'Drawing One', 'text': '', 'tags': '', 'url': 'Drawing One.html'}, {'title': 'Drawing Two', 'text': '', 'tags': '', 'url': 'Drawing Two.html'}]};
